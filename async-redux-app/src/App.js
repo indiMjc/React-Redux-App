6579fcbca5ffd538a6dafaba70d447ea2a840150
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-07 16:06:27 -0500

    added button that fetches top 10 exchanges and adds them to state

--- async-redux-app/src/App.js
+++ async-redux-app/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { getExchanges } from "./actions";
 import "./App.css";
 
 function App() {
+	const dispatch = useDispatch();
 	return (
 		<div className="App">
 			<h1>Top ten cryptocurrency exchanges by 24hr volume:</h1>
+			<button onClick={() => dispatch(getExchanges())}>Show top 10</button>
 		</div>
 	);
 }
