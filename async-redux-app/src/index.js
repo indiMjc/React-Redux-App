5fec2cf95f04b202429d67ab82ea6bc5319d0ee4
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-07 15:26:48 -0500

    redux dev tools set up and thunk added

--- async-redux-app/src/index.js
+++ async-redux-app/src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducers";
 
 import "./index.css";
 
-const store = createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
