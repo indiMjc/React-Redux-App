cb1b2aeb7a2b78874aa2e70c87d2e1e5df35e300
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-07 15:09:03 -0500

    empty reducer made, initial state setup, imports on index.js and more CRA cleanup

---
+++ async-redux-app/src/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+	exchange: "",
+	country: "",
+	url: "",
+	trustScore: null,
+	trustRank: null,
+	btcVolume: null,
+	usdVolume: null
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
