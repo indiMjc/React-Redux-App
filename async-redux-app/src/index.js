5a9c49a2420251c4bac43b3d37d1612e5a9fecc8
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-07 15:21:21 -0500

    empty store hooked up and provider wrapped around App

--- async-redux-app/src/index.js
+++ async-redux-app/src/index.js
@@ -4,8 +4,18 @@ import App from "./App";
 
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-// import { reducer } from "./reducers";
+import reducer from "./reducers";
 
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
--- async-redux-app/src/reducers/index.js
+++ async-redux-app/src/reducers/index.js
@@ -14,3 +14,5 @@ const reducer = (state = initialState, action) => {
 			return state;
 	}
 };
+
+export default reducer;
